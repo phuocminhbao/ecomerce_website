34717d8c6c1873d1442fa49bbedc38137183e3e5
Liam <liamnguyentks@gmail.com>
2021-07-24 15:48:26 +0700

    Update userview homepage

--- src/main/java/EcomerceWebsite/Controller/HomeController.java
+++ src/main/java/EcomerceWebsite/Controller/HomeController.java
@@ -2,11 +2,12 @@ package EcomerceWebsite.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/")
+	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
 	public String Index() {
-		return "index";
+		return "user/index";
 	}
 }
