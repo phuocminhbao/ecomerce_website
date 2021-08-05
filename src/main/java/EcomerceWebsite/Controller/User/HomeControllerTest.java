416b6da89611d673956752fc07bf030d2497c384
Huu Phuoc <s3804722@users.noreply.github.com>
2021-08-05 23:41:27 +0700

    travis Ci

---
+++ src/main/java/EcomerceWebsite/Controller/User/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package EcomerceWebsite.Controller.User;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeControllerTest extends HomeController {
+
+	@Test
+	public void testIndex() {
+		fail("Not yet implemented");
+	}
+
+}
