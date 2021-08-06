5173d27bb3193a96a98b71f1848fd346d2f4bb4d
Huu Phuoc <s3804722@users.noreply.github.com>
2021-08-06 21:04:02 +0700

    travis Ci setting database

--- travis.sql
+++ travis.sql
@@ -1,9 +1,9 @@
-reate Testuser
-CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
+
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'root'@'localhost';
 # Create DB
 CREATE DATABASE IF NOT EXISTS `ecomercewebsite` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `demo`;
+USE `ecomercewebsite`;
 
 --
 -- Database: `ecomercewebsite`
